028de5c92302a21c8bd91554d68c728de57729d4
Camille04w <vitoriacamille603@gmail.com>
2024-10-17 14:20:26 +0000

    aula1710

--- src/main/java/application/repository/PlataformaRepository.java
+++ src/main/java/application/repository/PlataformaRepository.java
@@ -1,5 +1,10 @@
 package application.repository;
-
-public class PlataformaRepository{
-    
+ 
+import org.springframework.data.jpa.repository.JpaRepository;
+ 
+import application.model.Plataforma;
+ 
+public interface PlataformaRepository extends JpaRepository<Plataforma, Long> {
+   
 }
+ 
